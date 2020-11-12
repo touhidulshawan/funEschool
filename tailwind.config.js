619d9573b0df79d0f4440020941885e736953b07
Touhidul Shawan <touhidulshawan@gmail.com>
2020-11-12 21:48:41 +0600

    purge unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
     extend: {},
   },
